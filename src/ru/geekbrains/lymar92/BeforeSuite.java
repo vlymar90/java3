e5ee94da283f783bfe5a96c2e003d3ea5f9cf246
vlymar90 <7ua.lymar2014@gmail.com>
2021-01-30 15:56:54 +0300

    done task lesson7

---
+++ src/ru/geekbrains/lymar92/BeforeSuite.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lymar92;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BeforeSuite {
+     int value() default  1;
+}
--- src/ru/geekbrains/lymar92/Orange.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.lymar92;
-
-public class Orange extends Fruit {
-
-    public Orange() {
-        super(1.5f);
-
-    }
-}
